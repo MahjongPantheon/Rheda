4311798e9b12005d584427ccf069cdabdc0eec12
Oleg Klimenko <o.klimenko@2gis.ru>
2016-11-13 14:36:40 +0700

    Add zero columns to hand value summary

--- src/controllers/Graphs.php
+++ src/controllers/Graphs.php
@@ -43,6 +43,11 @@ class Graphs extends Controller
 
             $handValueStats = [];
             $yakumanCount = 0;
+            $data['hands_value_summary'] += [
+                1 => 0, 2 => 0, 3 => 0, 4 => 0,
+                5 => 0, 6 => 0, 7 => 0, 8 => 0,
+                9 => 0, 10 => 0, 11 => 0, 12 => 0
+            ];
             foreach ($data['hands_value_summary'] as $han => $count) {
                 if ($han > 0) {
                     $handValueStats []= [(string)$han, $count];
